31d4df124cd3c5060174d92d30c939c148a14ef9
Ayushi <ayushi.maheshwari@q2ebanking.com>
2025-02-28 07:29:11 +0530

    6 and 7 commit

---
+++ pjt6_auto_suggestion_picklist/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Picklist from './components/Picklist'
+
+function App() {
+
+  return (
+    <>
+    <div className='start-app'>
+      <Picklist></Picklist>
+      </div>
+    </>
+  )
+}
+
+export default App
